f9a342c399ca72012ba5ba0e491f631c56555ddb
alex <earthhuman41@gmail.com>
2019-08-02 11:02:22 +0300

    Updates

---
+++ precache-manifest.b54e9423adc9d015b90b98306559ee3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b87b6b68af7512233bf5d21bd731c0",
+    "url": "/tabs-with-router/index.html"
+  },
+  {
+    "revision": "97331dc0baed9741075e",
+    "url": "/tabs-with-router/static/css/main.9501a29c.chunk.css"
+  },
+  {
+    "revision": "11414dd8f73ce36aad0a",
+    "url": "/tabs-with-router/static/js/2.8afd3ca4.chunk.js"
+  },
+  {
+    "revision": "97331dc0baed9741075e",
+    "url": "/tabs-with-router/static/js/main.64b3a16c.chunk.js"
+  },
+  {
+    "revision": "3513456d47be6d2df32f",
+    "url": "/tabs-with-router/static/js/runtime~main.8e076b95.js"
+  }
+]);
